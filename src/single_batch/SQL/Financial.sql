b5f322cff07bc3801105a13cdf2ffbff383a0cf6
shannon-barrow <shannon-barrow>
2024-06-06 14:24:19 +0000

    minor updates to how finwire is read

--- src/single_batch/SQL/Financial.sql
+++ src/single_batch/SQL/Financial.sql
@@ -1,58 +1,32 @@
 -- Databricks notebook source
+-- CREATE WIDGET DROPDOWN scale_factor DEFAULT "10" CHOICES SELECT * FROM (VALUES ("10"), ("100"), ("1000"), ("5000"), ("10000"));
+-- CREATE WIDGET TEXT tpcdi_directory DEFAULT "/Volumes/tpcdi/tpcdi_raw_data/tpcdi_volume/";
+-- CREATE WIDGET TEXT wh_db DEFAULT '';
+-- CREATE WIDGET TEXT catalog DEFAULT 'tpcdi';
+-- CREATE WIDGET DROPDOWN conameorcik DEFAULT "NAME" CHOICES SELECT * FROM (VALUES ("COMPANYID"), ("NAME"));
+
+-- COMMAND ----------
+
 INSERT INTO ${catalog}.${wh_db}_${scale_factor}.Financial
-WITH FIN as (
-  SELECT
-    to_timestamp(substring(value, 1, 15), 'yyyyMMdd-HHmmss') AS PTS,
-    cast(substring(value, 19, 4) AS INT) AS fi_year,
-    cast(substring(value, 23, 1) AS INT) AS fi_qtr,
-    to_date(substring(value, 24, 8), 'yyyyMMdd') AS fi_qtr_start_date,
-    cast(substring(value, 40, 17) AS DOUBLE) AS fi_revenue,
-    cast(substring(value, 57, 17) AS DOUBLE) AS fi_net_earn,
-    cast(substring(value, 74, 12) AS DOUBLE) AS fi_basic_eps,
-    cast(substring(value, 86, 12) AS DOUBLE) AS fi_dilut_eps,
-    cast(substring(value, 98, 12) AS DOUBLE) AS fi_margin,
-    cast(substring(value, 110, 17) AS DOUBLE) AS fi_inventory,
-    cast(substring(value, 127, 17) AS DOUBLE) AS fi_assets,
-    cast(substring(value, 144, 17) AS DOUBLE) AS fi_liability,
-    cast(substring(value, 161, 13) AS BIGINT) AS fi_out_basic,
-    cast(substring(value, 174, 13) AS BIGINT) AS fi_out_dilut,
-    nvl(string(try_cast(trim(substring(value, 187, 60)) as bigint)), trim(substring(value, 187, 60))) conameorcik
-  FROM ${catalog}.${wh_db}_${scale_factor}_stage.FinWire
-  WHERE rectype = 'FIN'
-),
-dc as (
-  SELECT 
-    sk_companyid,
-    name conameorcik,
-    EffectiveDate,
-    EndDate
-  FROM ${catalog}.${wh_db}_${scale_factor}.DimCompany
-  UNION ALL
-  SELECT 
-    sk_companyid,
-    cast(companyid as string) conameorcik,
-    EffectiveDate,
-    EndDate
-  FROM ${catalog}.${wh_db}_${scale_factor}.DimCompany
-)
 SELECT 
   sk_companyid,
-  fi_year,
-  fi_qtr,
-  fi_qtr_start_date,
-  fi_revenue,
-  fi_net_earn,
-  fi_basic_eps,
-  fi_dilut_eps,
-  fi_margin,
-  fi_inventory,
-  fi_assets,
-  fi_liability,
-  fi_out_basic,
-  fi_out_dilut
-FROM FIN
-JOIN dc 
+  cast(substring(value, 1, 4) AS INT) AS fi_year,
+  cast(substring(value, 5, 1) AS INT) AS fi_qtr,
+  to_date(substring(value, 6, 8), 'yyyyMMdd') AS fi_qtr_start_date,
+  cast(substring(value, 22, 17) AS DOUBLE) AS fi_revenue,
+  cast(substring(value, 39, 17) AS DOUBLE) AS fi_net_earn,
+  cast(substring(value, 56, 12) AS DOUBLE) AS fi_basic_eps,
+  cast(substring(value, 68, 12) AS DOUBLE) AS fi_dilut_eps,
+  cast(substring(value, 80, 12) AS DOUBLE) AS fi_margin,
+  cast(substring(value, 92, 17) AS DOUBLE) AS fi_inventory,
+  cast(substring(value, 109, 17) AS DOUBLE) AS fi_assets,
+  cast(substring(value, 126, 17) AS DOUBLE) AS fi_liability,
+  cast(substring(value, 143, 13) AS BIGINT) AS fi_out_basic,
+  cast(substring(value, 156, 13) AS BIGINT) AS fi_out_dilut
+FROM ${catalog}.${wh_db}_${scale_factor}_stage.FinWire
+JOIN ${catalog}.${wh_db}_${scale_factor}.DimCompany dc
 ON
-  FIN.conameorcik = dc.conameorcik 
-  AND date(PTS) >= dc.effectivedate 
-  AND date(PTS) < dc.enddate;
+  rectype = 'FIN_${conameorcik}'
+  and trim(substring(value, 169, 60)) = dc.${conameorcik}
+  AND recdate >= dc.effectivedate 
+  AND recdate < dc.enddate
