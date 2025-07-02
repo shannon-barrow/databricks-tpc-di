1e00bd7b4ae7e847c0b075e2fafd67e6c7639a73
shannon-barrow <shannon-barrow>
2025-07-02 13:13:01 +0000

    adding benchmark version of clustered parquet iceberg (no create or replace statement)

---
+++ src/single_batch/SQL_BENCH_PARQUET_CLUSTERED/FactCashBalances.sql
@@ -0,0 +1,33 @@
+-- Databricks notebook source
+INSERT OVERWRITE ${catalog}.${wh_db}_${scale_factor}.FactCashBalances
+with historical as (
+  SELECT
+    accountid,
+    to_date(ct_dts) datevalue,
+    sum(ct_amt) account_daily_total
+  FROM parquet.`${tpcdi_directory}sf=${scale_factor}/Batch1/CashTransaction*.parquet`
+  GROUP BY ALL
+),
+alltransactions as (
+  SELECT *, 1 batchid FROM historical
+  UNION ALL
+  SELECT
+    accountid,
+    to_date(ct_dts) datevalue,
+    sum(ct_amt) account_daily_total,
+    int(substring(_metadata.file_path FROM (position('/Batch', _metadata.file_path) + 6) FOR 1)) batchid
+  FROM parquet.`${tpcdi_directory}sf=${scale_factor}/Batch{2,3}/CashTransaction.parquet`
+  GROUP BY ALL
+)
+SELECT 
+  a.sk_customerid, 
+  a.sk_accountid, 
+  bigint(date_format(datevalue, 'yyyyMMdd')) sk_dateid,
+  sum(account_daily_total) OVER (partition by c.accountid order by datevalue) cash,
+  c.batchid
+FROM alltransactions c
+JOIN ${catalog}.${wh_db}_${scale_factor}.DimAccount a 
+  ON 
+    c.accountid = a.accountid
+    AND c.datevalue >= a.effectivedate 
+    AND c.datevalue < a.enddate
